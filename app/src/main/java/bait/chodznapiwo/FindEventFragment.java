c769a6ac2a0e42ac90473d7b9a1eae2e40497a75
Mateusz <matteuszz20@gmail.com>
2017-11-25 22:19:12 +0100

    detail_event

--- app/src/main/java/bait/chodznapiwo/FindEventFragment.java
+++ app/src/main/java/bait/chodznapiwo/FindEventFragment.java
@@ -32,11 +32,13 @@ public class FindEventFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_event_find, container, false);
         // Inflate the layout for this fragment
         List<Event> tmp= new ArrayList<Event>();
-        tmp.add(new Event("Pierwszy",new Timestamp(2010,7,7,7,7,0,0),"Olimp","00000",1,"testowy"));
-
+        for(int i=0;i<10;i++) {
+            tmp.add(new Event("Pierwszy",new Timestamp(2010,7,7,7,7,0,0),"Olimp","00000",1,"testowy"));
+        }
         mEventList= rootView.findViewById(R.id.event_list);
-        mEventAdapterAdapter =new EventAdapter(this.getActivity(),tmp);
-        ;
+
+            mEventAdapterAdapter = new EventAdapter(this.getActivity(), tmp);
+
         mEventList.setAdapter(mEventAdapterAdapter);
 
         return rootView;
