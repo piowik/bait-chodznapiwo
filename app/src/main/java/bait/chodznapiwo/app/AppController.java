2bacea3f20c0847de6705d88cc60f755553d981a
piowik <piotrwiklinski@gmail.com>
2017-11-25 21:12:02 +0100

    Registration done

--- app/src/main/java/bait/chodznapiwo/app/AppController.java
+++ app/src/main/java/bait/chodznapiwo/app/AppController.java
@@ -1,6 +1,14 @@
 package bait.chodznapiwo.app;
 
 import android.app.Application;
+import android.text.TextUtils;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.Volley;
+
+import java.util.ArrayList;
 
 /**
  * Created by Piotrek on 25.11.2017.
@@ -8,6 +16,7 @@ import android.app.Application;
 
 public class AppController extends Application {
 
+    private RequestQueue mRequestQueue;
     private static final String TAG = AppController.class
             .getSimpleName();
     private static AppController mInstance;
@@ -30,4 +39,30 @@ public class AppController extends Application {
         return mPrefManager;
     }
 
+
+    public RequestQueue getRequestQueue() {
+        if (mRequestQueue == null) {
+            mRequestQueue = Volley.newRequestQueue(getApplicationContext());
+        }
+
+        return mRequestQueue;
+    }
+
+
+    public <T> void addToRequestQueue(Request<T> req, String tag) {
+        // set the default tag if tag is empty
+        req.setTag(TextUtils.isEmpty(tag) ? TAG : tag);
+        getRequestQueue().add(req);
+    }
+
+    public <T> void addToRequestQueue(Request<T> req) {
+        req.setTag(TAG);
+        getRequestQueue().add(req);
+    }
+
+    public void cancelPendingRequests(Object tag) {
+        if (mRequestQueue != null) {
+            mRequestQueue.cancelAll(tag);
+        }
+    }
 }
---
+++ app/src/main/java/bait/chodznapiwo/app/Networking.java
@@ -0,0 +1,14 @@
+package bait.chodznapiwo.app;
+
+/**
+ * Created by Piotrek on 25.11.2017.
+ */
+
+public class Networking {
+    private static final String BASE = "http://bait.harryweb.pl/v1";
+    public static final String LOGIN = BASE + "/user/login";
+    public static final String REGISTER = BASE + "/user/register";
+    public static final String GET_USER = BASE + "/users/";
+    public static final String UPDATE_TOKEN = BASE + "/users/";
+
+}
