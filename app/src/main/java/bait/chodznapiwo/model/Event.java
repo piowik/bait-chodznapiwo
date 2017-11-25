656ee5a38ec1cc4438740c4c712ae09697e5d745
Karol Stypa <bury09@gmail.com>
2017-11-25 21:32:51 +0100

    Users list added to Event class

--- app/src/main/java/bait/chodznapiwo/model/Event.java
+++ app/src/main/java/bait/chodznapiwo/model/Event.java
@@ -1,6 +1,7 @@
 package bait.chodznapiwo.model;
 
 import java.sql.Timestamp;
+import java.util.LinkedList;
 
 /**
  * Created by karol on 11/25/17.
@@ -13,14 +14,16 @@ public class Event {
     private String mCoords;
     private int mEvent_type_id;
     private String mDescription;
+    private LinkedList<User> mUsers;
 
-    public Event(String mName, Timestamp mDate, String mPlace, String mCoords, int mEvent_type_id, String mDescription) {
+    public Event(String mName, Timestamp mDate, String mPlace, String mCoords, int mEvent_type_id, String mDescription, LinkedList<User> mUsers) {
         this.mName = mName;
         this.mDate = mDate;
         this.mPlace = mPlace;
         this.mCoords = mCoords;
         this.mEvent_type_id = mEvent_type_id;
         this.mDescription = mDescription;
+        this.mUsers = mUsers;
     }
     public String getName() {
         return mName;
